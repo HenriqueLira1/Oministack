f33a0e1182446e32f42b57051406cdecf919cff9
henrique <HenriqueLira1>
2019-08-11 01:55:36 -0300

    + Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const server = express();
+
+mongoose.connect('mongodb+srv://henrique:henrique@cluster0-jsaku.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333); 
