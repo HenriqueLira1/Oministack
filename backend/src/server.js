1d25a9c9fed3d8b442a884a39fc8b75131489428
henrique <HenriqueLira1>
2019-08-11 14:26:36 -0300

    + Add gitignore

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 const server = express();
 
 mongoose.connect('mongodb+srv://henrique:henrique@cluster0-jsaku.mongodb.net/omnistack?retryWrites=true&w=majority', {
